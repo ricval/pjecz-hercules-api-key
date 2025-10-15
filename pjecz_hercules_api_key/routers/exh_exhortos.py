e35844ecc498232d1404a8e5a798c77cd952bd59
ricval <rcvaldes@gmail.com>
2025-10-15 11:31:33 -0600

    Crear y Paginado de exhortos completo

--- pjecz_hercules_api_key/routers/exh_exhortos.py
+++ pjecz_hercules_api_key/routers/exh_exhortos.py
@@ -17,7 +17,7 @@ from ..config.settings import Settings, get_settings
 from ..dependencies.authentications import UsuarioInDB, get_current_active_user
 from ..dependencies.database import Session, get_db
 from ..dependencies.fastapi_pagination_custom_page import CustomPage
-from ..dependencies.safe_string import safe_clave
+from ..dependencies.safe_string import safe_clave, safe_string
 from ..models.autoridades import Autoridad
 from ..models.exh_areas import ExhArea
 from ..models.exh_exhortos import ExhExhorto
@@ -26,7 +26,7 @@ from ..models.exh_exhortos_partes import ExhExhortoParte
 from ..models.materias import Materia
 from ..models.municipios import Municipio
 from ..models.permisos import Permiso
-from ..schemas.exh_exhortos import ExhExhortoIn, ExhExhortoOut, OneExhExhortoOut
+from ..schemas.exh_exhortos import ExhExhortoIn, ExhExhortoOut, OneExhExhortoOut, ExhExhortoPaginadoOut
 from ..schemas.exh_exhortos_partes import ExhExhortoParteOut
 
 exh_exhortos = APIRouter(prefix="/api/v5/exh_exhortos", tags=["exhortos"])
@@ -62,7 +62,7 @@ async def detalle(
     )
 
 
-@exh_exhortos.get("", response_model=CustomPage[ExhExhortoOut])
+@exh_exhortos.get("", response_model=CustomPage[ExhExhortoPaginadoOut])
 async def paginado(
     current_user: Annotated[UsuarioInDB, Depends(get_current_active_user)],
     database: Annotated[Session, Depends(get_db)],
@@ -115,14 +115,14 @@ async def crear(
         return OneExhExhortoOut(success=False, message="Ese área no está activa")
 
     # Validar el municipio de origen
-    municipio_origen = database.query(Municipio).get(exh_exhorto_in.municipio_origen_id)
+    municipio_origen = database.query(Municipio).filter_by(clave = str(exh_exhorto_in.municipio_origen_id).zfill(3)).first()
     if municipio_origen is None:
         return OneExhExhortoOut(success=False, message="No existe ese municipio de origen")
     if municipio_origen.estatus != "A":
         return OneExhExhortoOut(success=False, message="Ese municipio está inactivo")
 
     # Validar el municipio de destino
-    municipio_destino = database.query(Municipio).get(exh_exhorto_in.municipio_destino_id)
+    municipio_destino = database.query(Municipio).filter_by(clave = str(exh_exhorto_in.municipio_destino_id).zfill(3)).first()
     if municipio_destino is None:
         return OneExhExhortoOut(success=False, message="No existe ese municipio de destino")
     if municipio_destino.estatus != "A":
@@ -145,6 +145,27 @@ async def crear(
         return OneExhExhortoOut(success=False, message='No existe el tipo de diligencia con clave "OTR"')
     if exh_tipo_diligencia.estatus != "A":
         return OneExhExhortoOut(success=False, message='El tipo de diligencia con clave "OTR" no está activo')
+
+    # Validar las Partes
+    for exh_exhorto_parte_in in exh_exhorto_in.exh_exhorto_partes:
+        parte_nombre = safe_string(exh_exhorto_parte_in.nombre)
+        if parte_nombre == "":
+            return OneExhExhortoOut(success=False, message='El nombre de una parte no es válido')
+        if exh_exhorto_parte_in.genero not in ExhExhortoParte.GENEROS:
+            return OneExhExhortoOut(success=False, message='El género de una parte no es válido')
+        if exh_exhorto_parte_in.tipo_parte not in ExhExhortoParte.TIPOS_PARTES:
+            return OneExhExhortoOut(success=False, message='El tipo de parte de una parte no es válido')
+        if exh_exhorto_parte_in.es_persona_moral == False:
+            parte_apellido_paterno = safe_string(exh_exhorto_parte_in.apellido_paterno)
+            if parte_apellido_paterno == "":
+                return OneExhExhortoOut(success=False, message='El apellido paterno de una persona física no es válido o está vacío')
+            parte_apellido_materno = safe_string(exh_exhorto_parte_in.apellido_materno)
+            if parte_apellido_materno == "":
+                return OneExhExhortoOut(success=False, message='El apellido materno de una persona física no es válido o está vacío')
+        if exh_exhorto_parte_in.tipo_parte_nombre == 0:
+            parte_nombre_parte = safe_string(exh_exhorto_parte_in.tipo_parte_nombre)
+            if parte_nombre_parte == "":
+                return OneExhExhortoOut(success=False, message='El nombre de una parte no es válido y es necesario')
 
     # Crear el exhorto
     exh_exhorto = ExhExhorto(
@@ -173,17 +194,27 @@ async def crear(
     for exh_exhorto_parte_in in exh_exhorto_in.exh_exhorto_partes:
         exh_exhorto_parte = ExhExhortoParte(
             exh_exhorto_id=exh_exhorto.id,
-            nombre=exh_exhorto_parte_in.nombre,
-            apellido_paterno=exh_exhorto_parte_in.apellido_paterno,
-            apellido_materno=exh_exhorto_parte_in.apellido_materno,
+            nombre=safe_string(exh_exhorto_parte_in.nombre),
+            apellido_paterno=safe_string(exh_exhorto_parte_in.apellido_paterno),
+            apellido_materno=safe_string(exh_exhorto_parte_in.apellido_materno),
             genero=exh_exhorto_parte_in.genero,
             es_persona_moral=exh_exhorto_parte_in.es_persona_moral,
-            tipo_parte=exh_exhorto_parte_in.tipo_parte,
-            tipo_parte_nombre=exh_exhorto_parte_in.tipo_parte_nombre,
+            tipo_parte=int(exh_exhorto_parte_in.tipo_parte),
+            tipo_parte_nombre=safe_string(exh_exhorto_parte_in.tipo_parte_nombre),
         )
         database.add(exh_exhorto_parte)
-        database.commit()
-        database.refresh(exh_exhorto_parte)
+    # Inserción de las partes
+    database.commit()
+    # Actualizar el resultado del exhorto
+    database.refresh(exh_exhorto)
+    # Obtener las partes
+    exh_exhortos_partes = database.query(ExhExhortoParte).filter(ExhExhortoParte.exh_exhorto_id == exh_exhorto.id).all()
+    if exh_exhortos_partes is None:
+        return OneExhExhortoOut(success=False, message="No existen partes en este exhorto")
+    partes = []
+    for parte in exh_exhortos_partes:
+        partes.append(ExhExhortoParteOut.model_validate(parte))
+    exh_exhorto.exh_exhorto_partes = partes
 
     # Entregar
     return OneExhExhortoOut(
